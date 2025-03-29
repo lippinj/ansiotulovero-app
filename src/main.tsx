6123bdfdcfc4cf8ffe0e005643215d76fbbe39f9
Joonas Lipping <lippinj@outlook.com>
2025-03-29 23:21:56 +0200

    Update main

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "./index.css";
 
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+const router = createBrowserRouter([{ path: "/", element: <App /> }], {
+  basename: "/",
+});
+
+const root = createRoot(document.getElementById("root")!);
+root.render(<RouterProvider router={router} />);
