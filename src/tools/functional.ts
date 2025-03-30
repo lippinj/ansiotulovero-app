c4c49a248a63934125985fd1da441fa16983d692
Joonas Lipping <lippinj@outlook.com>
2025-03-30 11:40:14 +0300

    Add functional.ts

---
+++ src/tools/functional.ts
@@ -0,0 +1,8 @@
+export function mapValues<V, V2>(
+  record: Record<string, V>,
+  pred: (value: V) => V2,
+): Record<string, V2> {
+  return Object.fromEntries(
+    Object.entries(record).map(([k, v]) => [k, pred(v as V)]),
+  );
+}
